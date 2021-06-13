d4470921e1cc602491c6ed1707a9792f519d2f49
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-06-13 13:21:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+
+// // // // // // // // // // // // // 
+
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date?", function (req, res) {
+  var parsedDate;
+  if(req.params.date === undefined) {
+    parsedDate = new Date(Date.now());        
+  } else {
+    parsedDate = new Date(req.params.date);
+    if(Number.isNaN(parsedDate.valueOf())){
+      parsedDate = new Date(parseInt(req.params.date));
+    }
+  }
+
+  console.log(parsedDate);
+  if(Number.isNaN(parsedDate.valueOf())){
+    res.json({ error : "Invalid Date" });
+  } else {
+    res.json({ unix: parsedDate.valueOf(), utc: parsedDate.toUTCString()});
+  }
+});
+
+
+
+// // // // // // // // // // // // // 
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
